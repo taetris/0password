dc2714b948a16da6728c617ae66510cab01008a0
taetris <68907534+taetris@users.noreply.github.com>
2024-02-11 08:22:56 +0545

    updated dbconnect

--- dbconnect.py
+++ dbconnect.py
@@ -4,7 +4,7 @@ from pymongo import MongoClient
 def store_in_mongodb(credentials):
     """Store credentials in MongoDB."""
     
-    cluster = MongoClient("mongodb+srv://076bei047tripti:u3z1Sfx0JieW6yNG@clusteruno.ds5kfqa.mongodb.net/")
+    cluster = ""
     db = cluster["2password"]
     collection = db["trial-1"]
     collection.insert_many(credentials)
